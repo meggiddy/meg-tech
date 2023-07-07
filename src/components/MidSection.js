920906e4d2000c896e3f4bc6dc632d727bbb13d3
Maggie Muriithi <muriithinyawira13@gmail.com>
2023-07-07 16:25:03 +0300

    working on animation

--- src/components/MidSection.js
+++ src/components/MidSection.js
@@ -1,11 +1,16 @@
 function MidSection() {
-    return ( <>
-        <div>
-            <div>
-                <h2>My art comes from my happiest place which is pink, bubbly and fun.</h2>
-            </div>
+  return (
+    <>
+      <div className="h-40">
+        <div className="h-40">
+        <img alt="couple" src="../public/assets/animations/coupledog.png"></img>
+          <h2>
+            My art comes from my happiest place which is pink, bubbly and fun.
+          </h2>
         </div>
-    </> );
+      </div>
+    </>
+  );
 }
 
 export default MidSection;

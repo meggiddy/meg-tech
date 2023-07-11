5592aedfeb44571e8f5776e2feb65b1298605fc0
Maggie Muriithi <muriithinyawira13@gmail.com>
2023-07-11 09:59:47 +0300

    worked on bird animation

---
+++ src/animations/bird.js
@@ -0,0 +1,27 @@
+import "./bird.scss";
+
+function Birds() {
+  return (
+    <div className="container">
+      <h1>Meg the Dev</h1>
+
+      <div className="bird-container bird-container--one">
+        <div className="bird bird--one"></div>
+      </div>
+
+      <div className="bird-container bird-container--two">
+        <div className="bird bird--two"></div>
+      </div>
+
+      <div className="bird-container bird-container--three">
+        <div className="bird bird--three"></div>
+      </div>
+
+      <div className="bird-container bird-container--four">
+        <div className="bird bird--four"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Birds;
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,13 +1,18 @@
+import Birds from "../animations/bird";
+
 function Services() {
   return (
     <>
-      <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img
-            src="/assets/doodles/ladies.jpg"
-            className="w-full"
-            alt="art"
-          />
+      <div className="carousel w-full pt-10">
+        <div
+          id="slide1"
+          className="carousel-item flex flex-row justify-center relative w-full"
+        >
+          <p className="items-center w-80 flex p-10 text-3xl">
+            Art is expressive
+          </p>
+          <img src="/assets/doodles/ladies.jpg" className="w-96" alt="art" />
+
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide4" className="btn btn-circle">
               ❮
@@ -17,12 +22,18 @@ function Services() {
             </a>
           </div>
         </div>
-        <div id="slide2" className="carousel-item relative w-full">
+        <div
+          id="slide2"
+          className="carousel-item flex flex-row justify-center relative w-full"
+        >
           <img
             src="/assets/doodles/woman-doodle.jpg"
-            className="w-full"
+            className="w-96"
             alt="art"
           />
+          <p className="items-center w-80 flex p-10 text-3xl">
+            Art is expressive
+          </p>
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide1" className="btn btn-circle">
               ❮
@@ -32,12 +43,14 @@ function Services() {
             </a>
           </div>
         </div>
-        <div id="slide3" className="carousel-item relative w-full">
-          <img
-            src="/assets/doodles/doodle.jpg"
-            className="w-full"
-            alt="art"
-          />
+        <div
+          id="slide3"
+          className="carousel-item flex flex-row justify-center relative w-full"
+        >
+          <p className="items-center w-80 flex p-10 text-3xl">
+            Art is expressive
+          </p>
+          <img src="/assets/doodles/doodle.jpg" className="w-96" alt="art" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide2" className="btn btn-circle">
               ❮
@@ -47,12 +60,14 @@ function Services() {
             </a>
           </div>
         </div>
-        <div id="slide4" className="carousel-item relative w-full">
-          <img
-            src="/assets/doodles/painting.jpg"
-            className="w-full"
-            alt="art"
-          />
+        <div
+          id="slide4"
+          className="carousel-item flex flex-row justify-center relative w-full"
+        >
+          <p className="items-center w-80 flex p-10 text-3xl">
+            Art is expressive
+          </p>
+          <img src="/assets/doodles/painting.jpg" className="w-96" alt="art" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide3" className="btn btn-circle">
               ❮
@@ -63,6 +78,7 @@ function Services() {
           </div>
         </div>
       </div>
+      <Birds />
     </>
   );
 }
